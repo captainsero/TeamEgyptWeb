b53367832c560c382d7e2d4ef6ee7f758940c9a0
Captain Sero <yasoo976098@gmail.com>
2025-10-15 10:32:12 +0300

    solve keyboard overloading, fix 1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "767190565"
+    serviceWorkerVersion: "3413431365"
   }
 });
